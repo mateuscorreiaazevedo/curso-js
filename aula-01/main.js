5965c079d856f2f329c2f39a66f0cf77148fc8c7
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-20 16:19:01 -0300

    aula 2 - tipos primitivos

---
+++ aula-01/main.js
@@ -0,0 +1,31 @@
+// Vars
+var name = "";
+var sexo = "";
+if (!name, !sexo) {
+  var name = "Clara";
+  var sexo = "masc";
+  console.log(name);
+  console.log(sexo);
+}
+
+var sexo = "fem"
+
+console.log(name);
+console.log(sexo)
+
+// Let
+let nome = ""
+
+if (!nome) {
+    nome = "clarinha meu amor"
+    let genre = "feminino"
+    console.log(nome)
+    console.log(genre)
+}
+
+// console.log(genre) - O let se estiver dentro de uma condição, não poderá ser utilizado fora dela
+
+// Const
+const NAME = 'Mateus'
+console.log(NAME)
+ 
