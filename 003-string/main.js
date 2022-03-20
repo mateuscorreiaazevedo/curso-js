498fcf869bab4d69d0e6f8c6270844ee16a0a04b
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-20 16:59:53 -0300

    string finalizado

---
+++ 003-string/main.js
@@ -0,0 +1,15 @@
+// Tipo String
+// Conjunto de Caracteres
+// Pode usar para definir as "", '' ou ``
+let name = "Mateus"
+let age = "22"
+let dad = 'Waldemar'
+// console.log(typeof name, name)
+// console.log(typeof age, age)
+// console.log(typeof dad, dad)
+
+// toString
+
+// let frase = `${name} tem ${age} anos, filho de ${dad}`
+
+console.log (age + 1)
