b2b5c36f19196b4ec92c01aa137d15f839a61032
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-20 16:35:39 -0300

    tipo string

---
+++ 002-tipo-primitivo/main.js
@@ -0,0 +1,24 @@
+// Tipos primitivos são dados que não são representados através de um Objeto e, por consequência, não possui métodos.
+// Podem ser:
+// string
+// number
+// boolean
+// null
+// undefined
+
+
+// String
+let nome = "Mateus Azevedo"
+console.log(typeof nome,": ", nome)
+// Number
+let idade = 22
+console.log(typeof idade,": ", idade)
+// Boolean
+let boolean = true
+console.log(typeof boolean,": ", boolean)
+// Null
+let nulo = null
+console.log(typeof nulo, ": ", nulo)
+// Undefined
+let under
+console.log(typeof under, ": ", under)
