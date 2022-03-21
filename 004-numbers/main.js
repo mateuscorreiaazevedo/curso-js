5f9ee31973cb3a6a7e99fa55fa41eeb4ed7e7e8c
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-20 23:34:10 -0300

    tipo numbers finalizado, iniciando operadores aritméticos

---
+++ 004-numbers/main.js
@@ -0,0 +1,25 @@
+// Tipo Numbers
+// parseInt
+let idade = "0+1"
+console.log(parseInt(idade))
+// parseFloat
+// console.log(parseFloat(idade))
+
+
+
+let age = Number("22")
+
+// console.log(typeof age, age)
+
+// boolean em 0 ou 1
+
+let number = Number(true) /* 1 */
+let number1 = Number(false) /* 0 */
+
+// console.log(typeof number)
+// console.log("number: " + number)
+// console.log("number1: "+number1)
+
+let num = new Date()
+let num2 = Number(new Date())
+// console.log(num2)
