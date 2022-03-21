be19de4a7b81406c681f78bd9715c0461b4da981
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-21 00:31:41 -0300

    operação de comparação finalizado

---
+++ 007-operadores-comparacao/main.js
@@ -0,0 +1,16 @@
+// Operadores de COMPARAÇÃO
+
+let idade = 22
+// Comparação boolean (true or false)
+// Maior que
+console.log(idade > 30)
+// Menor que
+console.log(`É menor que: ${idade < 30}`)
+// Igual
+console.log (idade == 22)
+// igual em string (vai ser verdadeiro)
+console.log(`Igual em string: ${idade == "22"}`)
+// igualdade restrita em string (vai ser falso)
+console.log(`Igual em string: ${idade === "22"}`)
+// Diferente
+console.log(idade != 22)
