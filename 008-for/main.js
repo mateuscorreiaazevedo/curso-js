18393d2c4d30d6ad0222fb9c11706a9ef90445f9
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-21 00:55:53 -0300

    for finalizado

---
+++ 008-for/main.js
@@ -0,0 +1,10 @@
+// For - sistema de repetições
+const FRUTAS = ["banana", "pera", "uva", "maracujá", "morango"];
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i])
+// }
+
+FRUTAS.forEach((fruta, index) => {
+    console.log(index +" "+ fruta)
+});
