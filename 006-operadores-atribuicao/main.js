a5efa77764a5f166a9076e1c646ca7b632c269c9
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-21 00:06:36 -0300

    operações de atribuição finalizado

---
+++ 006-operadores-atribuicao/main.js
@@ -0,0 +1,17 @@
+// OPERADORES DE ATRIBUIÇÃO
+// Operador básico "="
+let value = 1
+console.log(`valor inicial: ${value}`)
+// += soma o valor inicial atribuido com a nova atribuição
+value += 2
+console.log(`valor somado: ${value}`)
+
+// -= subtrai o valor inicial atribuido pela nova atribuição
+value -= 1
+console.log(`valor subtraído: ${value}`)
+/* *= multiplica o valor inicial atribuido pela nova atribuição */
+value *= 10
+console.log(`valor multiplicado: ${value}`)
+//  /= divide o valor inicial atribuido pela nova atribuição
+value /= 5
+console.log(`valor dividido: ${value}`)
