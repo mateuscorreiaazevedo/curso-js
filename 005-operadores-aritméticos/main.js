09ffe76f91ee9c991c70b3ddcdd62e60c94f3050
mateuscorreiaazevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-03-20 23:55:17 -0300

    operadores aritméticos finalizado

---
+++ 005-operadores-aritméticos/main.js
@@ -0,0 +1,22 @@
+// Operadores Aritméticos!
+//      Adição
+//      Subtração
+//      Divisão
+//      Multiplicação
+
+// Soma
+let v1 = 1
+let v2 = 2
+console.log(`Adição: ${v2 + v1}`)
+// Subtração
+let v3 = 1
+let v4 = 2
+console.log(`Subtração: ${v4 - v3}`)
+// Multiplicação
+let v5 = 3
+let v6 = 2
+console.log(`Multiplicação: ${v6 * v5}`)
+// Divisão
+let v7 = 10
+let v8 = 2
+console.log(`Divisão: ${v7 / v8}`)
