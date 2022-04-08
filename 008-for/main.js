5d476c6650c2add2bd235380342aae9677cad555
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-04-08 01:16:22 -0300

    sdsa

--- 008-for/main.js
+++ 008-for/main.js
@@ -1,10 +1,6 @@
 // For - sistema de repetições
 const FRUTAS = ["banana", "pera", "uva", "maracujá", "morango"];
 
-// for (let i = 0; i < frutas.length; i++) {
-//     console.log(frutas[i])
-// }
-
-FRUTAS.forEach((fruta, index) => {
-    console.log(index +" "+ fruta)
-});
+for (let i = 0; i < FRUTAS.length; i++) {
+    console.log(FRUTAS[i])
+}
